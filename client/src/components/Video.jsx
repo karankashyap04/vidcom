1ea0522bdbecc49812b7307da06c3711d799cb7d
karankashyap04 <karankashyap@gmail.com>
2023-01-04 08:56:43 +0530

    feat: Display video-streams in real-time

--- client/src/components/Video.jsx
+++ client/src/components/Video.jsx
@@ -1,9 +1,49 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Typography, Paper } from "@material-ui/core";
+import "../styles/Video.css";
+import { SocketContext } from "../SocketContext";
 
 export default function Video() {
+  const {
+    myVideo,
+    otherVideo,
+    username,
+    stream,
+    call,
+    isCallAccepted,
+    isCallEnded,
+  } = useContext(SocketContext);
   return (
-    <div>
-      <p>This is the Video playing component</p>
+    <div className="container">
+      <div className="row">
+        {stream ? (
+          <VideoDisplay videoRef={myVideo} isMuted={true} username={username} />
+        ) : null}
+        {isCallAccepted && !isCallEnded ? (
+          <VideoDisplay
+            videoRef={otherVideo}
+            isMuted={false}
+            username={call.callerName}
+          />
+        ) : null}
+      </div>
+    </div>
+  );
+}
+
+function VideoDisplay({ videoRef, isMuted, username }) {
+  return (
+    <div className="col col-lg-6 col-md-6 col-sm-12">
+      <Paper className="video-container">
+        <Typography variant="h5" gutterBottom>
+          {username || "Name"}
+        </Typography>
+        {isMuted ? (
+          <video ref={videoRef} playsInline autoPlay className="video" muted />
+        ) : (
+          <video ref={videoRef} playsInline autoPlay className="video" />
+        )}
+      </Paper>
     </div>
   );
 }
